5960cc5f5e13d16f2ffb43df4d0934a862dcbbb3
Rodolfo1705 <roabrahao5@gmail.com>
2023-08-05 10:14:03 -0300

    feat: adding utils and exercise 2

--- exercicios-nivelamento/lista-1/src/Exercises/Exercicio2.java
+++ exercicios-nivelamento/lista-1/src/Exercises/Exercicio2.java
@@ -1,9 +1,25 @@
 package Exercises;
 
+import Utils.Divisor;
+import Utils.InputHandler;
+
 public class Exercicio2 {
     public static void run(){
+        Divisor.printExercise(2);
 
+        int firstValue = InputHandler.getIntInput("Digite um valor: ");
+        int secondValue = InputHandler.getIntInput("Digite outro valor: ");
+
+        int higherValue = getHigherValue(firstValue, secondValue);
+
+        System.out.println("O maior valor entre " + firstValue + " e " + secondValue + " é: " + higherValue);
     }
 
-    private static
+    private static int getHigherValue(int firstValue, int secondValue){
+        int higherValue = firstValue;
+
+        if (secondValue > firstValue) higherValue = secondValue;
+
+        return higherValue;
+    }
 }
---
+++ exercicios-nivelamento/lista-1/src/Utils/InputHandler.java
@@ -0,0 +1,13 @@
+package Utils;
+
+import java.util.Scanner;
+
+public class InputHandler {
+    public static int getIntInput(String message){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(message);
+        int input = scanner.nextInt();
+
+        return input;
+    }
+}
